be4f4dfc773dc903a1bdcdf625304aa579c3897e
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-01-10 15:53:45 +0200

    Add endpoint to get GDPR mandated things (we have email addresses in our db)

--- src/arkia11napi/api.py
+++ src/arkia11napi/api.py
@@ -1,19 +1,41 @@
 """Main API entrypoint"""
 from typing import Mapping
+from pathlib import Path
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.responses import Response
 
 from .views.tokens import TOKEN_ROUTER
 from .views.roles import ROLE_ROUTER
 from .views.users import USER_ROUTER
 
+STATIC_PATH = Path(__file__).parent / "staticfiles"
+TEMPLATES_PATH = Path(__file__).parent / "templates"
+
 APP = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
+APP.mount("/static", StaticFiles(directory=str(STATIC_PATH)), name="static")
+TEMPLATES = Jinja2Templates(directory=str(TEMPLATES_PATH))
 APP.include_router(ROLE_ROUTER)
 APP.include_router(USER_ROUTER)
 APP.include_router(TOKEN_ROUTER)
 
 
-@APP.get("/api/v1")
+@APP.get("/gdpr", tags=["privacy"], response_class=HTMLResponse)
+async def show_gdpr(request: Request) -> Response:
+    """Show the GDPR info as HTML"""
+    return TEMPLATES.TemplateResponse(
+        "gdpr.html",
+        {
+            "request": request,
+            # FIXME: Add any config variables the template needs here
+        },
+    )
+
+
+@APP.get("/api/v1", tags=["misc"])
 async def hello() -> Mapping[str, str]:
     """Say hello"""
     return {"message": "Hello World"}
