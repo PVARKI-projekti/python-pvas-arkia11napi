f9ee9a1d9a0169a8d4819845851c8118b973f0f0
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-01-10 21:50:52 +0200

    Make it possible to override the templates paths

--- src/arkia11napi/config.py
+++ src/arkia11napi/config.py
@@ -1,5 +1,9 @@
 """General configuration variables"""
 from pathlib import Path
 
-STATIC_PATH = Path(__file__).parent / "staticfiles"
-TEMPLATES_PATH = Path(__file__).parent / "templates"
+from starlette.config import Config
+
+cfg = Config(".env")
+
+STATIC_PATH: Path = cfg("STATIC_PATH", cast=Path, default=Path(__file__).parent / "staticfiles")
+TEMPLATES_PATH: Path = cfg("TEMPLATES_PATH", cast=Path, default=Path(__file__).parent / "templates")
