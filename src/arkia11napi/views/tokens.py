c9d0b76ea23ba7f473810a4521449256c8731cd9
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-01-15 00:10:16 +0200

    untested token request

--- src/arkia11napi/views/tokens.py
+++ src/arkia11napi/views/tokens.py
@@ -1,9 +1,12 @@
 """Token relaed endpoints"""
+from typing import cast
 import logging
 
 import pendulum
-from fastapi import APIRouter, Depends
+from libadvian.binpackers import uuid_to_b64
+from fastapi import APIRouter, Depends, Request
 from fastapi.responses import RedirectResponse, FileResponse
+from fastapi_mail import MessageSchema, MessageType
 from starlette import status
 from arkia11nmodels.schemas.token import TokenRequest, DBToken
 from arkia11nmodels.models import Token, User
@@ -11,6 +14,7 @@ from arkia11nmodels.models import Token, User
 from ..schemas.tokens import TokenRequestResponse, TokenPager
 from ..helpers import get_or_404
 from ..security import JWTHandler, JWTBearer, JWTPayload, check_acl
+from ..mailer import singleton as getmailer
 
 LOGGER = logging.getLogger(__name__)
 TOKEN_ROUTER = APIRouter()
@@ -26,16 +30,47 @@ async def public_key() -> FileResponse:
     "/api/v1/tokens", tags=["tokens"], response_model=TokenRequestResponse, status_code=status.HTTP_201_CREATED
 )
 async def request_token(
-    req: TokenRequest, jwt: JWTPayload = Depends(JWTBearer(auto_error=False))
+    tkreq: TokenRequest, request: Request, jwt: JWTPayload = Depends(JWTBearer(auto_error=False))
 ) -> TokenRequestResponse:
     """Request a token"""
-    # FIXME: implement (for optional auth check acl if they can see the result)
-    _ = jwt
-    _ = req
+    if tkreq.deliver_via != "email":
+        return TokenRequestResponse(sent=False, errordetail="Only email delivery supported ATM")
+
+    user = await User.query.where(getattr(User, tkreq.deliver_via) == tkreq.target).gino.first()
+    if not user:
+        LOGGER.info("Could not find matching user for {}".format(tkreq))
+        if not check_acl(jwt, "fi.arki.arkia11nmodels.token:read", auto_error=False):
+            return TokenRequestResponse(sent=False, errordetail="User not found")
+        return TokenRequestResponse(sent=True)
+
+    user = cast(User, user)
+    # FIXME: figure what to do with audit_meta
+    token = Token.for_user(user)
+    send_to = getattr(user, tkreq.deliver_via)
+    token.sent_to = send_to
+    # FIXME redirect etc, check all props
+    await token.create()
+    token = Token.get(token.pk)
+    token_url = request.url_for("use_token", token=uuid_to_b64(token.pk))  # type: ignore # false positive
+
+    mailer = getmailer()
+    msg = MessageSchema(
+        subject="PVARKI login token",  # FIXME: make configurable in the request or env based default
+        recipients=[send_to],
+        subtype=MessageType.plain,
+        template_body={"login_url": token_url},
+    )
+    try:
+        await mailer.send_message(msg, template_name="token_email.txt")
+    except Exception as exc:  # pylint: disable=W0703
+        LOGGER.exception("mail delivery failure {}".format(exc))
+        await token.delete()  # completely remove tokens whose delivery failed
+        return TokenRequestResponse(sent=False, errordetail="Email send failed")
+
     return TokenRequestResponse(sent=True)
 
 
-@TOKEN_ROUTER.get("/api/v1/tokens/use", tags=["tokens"], response_class=RedirectResponse)
+@TOKEN_ROUTER.get("/api/v1/tokens/use", tags=["tokens"], response_class=RedirectResponse, name="use_token")
 @TOKEN_ROUTER.post("/api/v1/tokens/use", tags=["tokens"], response_class=RedirectResponse)
 async def use_token(token: str) -> RedirectResponse:
     """Use a token"""
