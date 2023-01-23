8da144efa441a2c8f45a1e5cc44925766806a423
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-01-23 18:16:25 +0200

    add cookie support to our JWT auth handler

--- src/arkia11napi/security.py
+++ src/arkia11napi/security.py
@@ -20,6 +20,8 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from arkia11nmodels.schemas.role import ACLItem
 from arkia11nmodels.models import User
 
+from .config import JWT_COOKIE_NAME
+
 LOGGER = logging.getLogger(__name__)
 JWT_LIFETIME = 60 * 60 * 2  # 2 hours in seconds
 
@@ -105,18 +107,22 @@ class JWTBearer(HTTPBearer):  # pylint: disable=R0903
     """Check JWT bearer tokens"""
 
     async def __call__(self, request: Request) -> Optional[JWTPayload]:  # type: ignore[override]
-        # FIXME: add cookie/query_param support
-        credentials: Optional[HTTPAuthorizationCredentials] = await super().__call__(request)
-        if not credentials:
-            # autp-error will have raised already if no auth header
-            return None
-        if credentials.scheme != "Bearer":
-            raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
+        jwt_b64: Optional[str] = None
         payload: Optional[Mapping[str, Any]] = None
+        if cookie := request.cookies.get(JWT_COOKIE_NAME):
+            jwt_b64 = cookie
+        else:
+            credentials: Optional[HTTPAuthorizationCredentials] = await super().__call__(request)
+            if not credentials:
+                # autp-error will have raised already if no auth header
+                return None
+            if credentials.scheme != "Bearer":
+                raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
+            jwt_b64 = credentials.credentials
         try:
-            payload = JWTHandler.singleton().decode(credentials.credentials)
+            payload = JWTHandler.singleton().decode(jwt_b64)
         except Exception as exc:  # pylint: disable=W0703
-            LOGGER.exception("Got problem {} decoding {}".format(exc, credentials))
+            LOGGER.exception("Got problem {} decoding {}".format(exc, jwt_b64))
         if not payload and self.auto_error:
             raise HTTPException(status_code=403, detail="Invalid or expired token.")
         # Inject into request state to avoid Repeating Myself
