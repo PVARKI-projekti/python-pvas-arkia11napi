8196929a2551b402f1750696bd1947e03650d8db
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-02-03 20:45:18 +0200

    test token use (custom redirect)

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -91,3 +91,42 @@ async def test_use_token_defaultredirect(unauth_client: TestClient, enduser_obje
         assert resp_use.status_code == 303
         assert JWT_COOKIE_NAME in resp_use.cookies
         assert resp_use.headers["location"].endswith("/api/v1/users/me")
+
+
+@pytest.mark.asyncio
+async def test_use_token_customredirect(unauth_client: TestClient, enduser_object: User) -> None:
+    """Test token use"""
+    mailer = arkia11napi.mailer.singleton()
+    with mailer.record_messages() as outbox:
+        resp_create = await unauth_client.post(
+            "/api/v1/tokens",
+            json={
+                "deliver_via": "email",
+                "target": enduser_object.email,
+                "redirect": "https://customurl.example.com",
+            },
+        )
+        LOGGER.debug("resp_create={}, content={}".format(resp_create, resp_create.content))
+        assert resp_create.status_code == 201
+        assert len(outbox) == 1
+        msg = outbox[0]
+        assert msg["subject"] == TOKEN_EMAIL_SUBJECT
+        # Fscking MIME...
+        email_body = ensure_str(outbox[0].get_payload(0).get_payload(decode=True))
+        assert "/api/v1/tokens/use" in email_body
+        LOGGER.debug("email_body={}".format(email_body))
+        match = re.search(r"\s(?P<url>https?://[^/]+(?P<path>\S+)\?token=(?P<token>\S+))\s", email_body)
+        LOGGER.debug("match={}".format(match))
+        if not match:
+            raise RuntimeError("Could not parse token/url")
+        token_uuid = b64_to_uuid(match.group("token"))
+        assert token_uuid
+
+        get_url = f"{match.group('path')}?token={match.group('token')}"
+        LOGGER.debug("GETting url {}".format(get_url))
+        resp_use = await unauth_client.get(get_url, allow_redirects=False)
+        LOGGER.debug("resp_use={}, .content={}".format(resp_use, resp_use.content))
+
+        assert resp_use.status_code == 303
+        assert JWT_COOKIE_NAME in resp_use.cookies
+        assert resp_use.headers["location"].startswith("https://customurl.example.com")
