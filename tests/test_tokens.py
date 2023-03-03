dd20485943d9a16ba6b81198352a3aeadcd30dbd
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-03-03 22:35:10 +0200

    test consume with redirect

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -197,3 +197,45 @@ async def test_consume_token_defaultredirect(
         assert parsed.expires
         assert parsed.refresh_url
         assert parsed.redirect is None
+
+
+@pytest.mark.asyncio
+async def test_consume_token_customredirect(
+    unauth_client: TestClient, enduser_object: User, overridden_token_url: str
+) -> None:
+    """Test token consume API with redirect"""
+    mailer = arkia11napi.mailer.singleton()
+    with mailer.record_messages() as outbox:
+        resp_create = await unauth_client.post(
+            "/api/v1/tokens",
+            json={
+                "deliver_via": "email",
+                "target": enduser_object.email,
+                "redirect": "https://customurl2.example.com",
+            },
+        )
+        LOGGER.debug("resp_create={}, content={}".format(resp_create, resp_create.content))
+        assert resp_create.status_code == 201
+        assert len(outbox) == 1
+        msg = outbox[0]
+        assert msg["subject"] == TOKEN_EMAIL_SUBJECT
+        # Fscking MIME...
+        email_body = ensure_str(outbox[0].get_payload(0).get_payload(decode=True))
+        assert overridden_token_url in email_body
+        LOGGER.debug("email_body={}".format(email_body))
+        match = re.search(r"\s(?P<url>https?://[^/]+(?P<path>\S+)\?token=(?P<token>\S+))\s", email_body)
+        LOGGER.debug("match={}".format(match))
+        if not match:
+            raise RuntimeError("Could not parse token/url")
+        token_uuid = b64_to_uuid(match.group("token"))
+        assert token_uuid
+        LOGGER.debug("token_uuid={}".format(token_uuid))
+
+        resp_use = await unauth_client.post("/api/v1/tokens/consume", json={"token": match.group("token")})
+        LOGGER.debug("resp_use={}, .content={}".format(resp_use, resp_use.content))
+        payload = resp_use.json()
+        parsed = TokenConsumeResponse.parse_obj(payload)
+        assert parsed.jwt
+        assert parsed.expires
+        assert parsed.refresh_url
+        assert parsed.redirect == "https://customurl2.example.com"
