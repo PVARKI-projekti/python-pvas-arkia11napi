eaca8588b48a7df193645687eff1e53fc271d5c1
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-03-05 08:26:05 +0200

    bump version

--- src/arkia11napi/__init__.py
+++ src/arkia11napi/__init__.py
@@ -1,2 +1,2 @@
 """ FastAPI for PVARKI user management, authentication and authorization """
-__version__ = "1.2.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
+__version__ = "1.2.1"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
